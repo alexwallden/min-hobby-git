9acd023233cab771cd9a1568fdd25063783a51d5
alexwallden <alexander.wallden@me.com>
2023-02-21 12:42:31 +0100

    added Header component and installed vue router

---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    setup() {
+        return {};
+    },
+});
+</script>
+
+<template>
+    <div>
+      <h1>Min hobby</h1>
+      <nav>
+        <ul>
+          <li>Start</li>
+          <li>Elbasen</li>
+          <li>Musiken</li>
+        </ul>
+      </nav>
+    </div>
+</template>
+
+<style scoped></style>
