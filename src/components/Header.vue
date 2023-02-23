4344170ba9f997f886ce7ea524c198ad00c2c449
alexwallden <alexander.wallden@me.com>
2023-02-23 08:58:01 +0100

    changed icon to svg

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -12,7 +12,31 @@ export default defineComponent({
 
 <template>
   <header>
-    <img src="../assets/logo-transparent-cropped.png" alt="Elbas" height="77">
+    <!-- <img src="../assets/logo-transparent-cropped.png" alt="Elbas" height="77"> -->
+    <svg
+      fill="#000000"
+      version="1.1"
+      id="Capa_1"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlns:xlink="http://www.w3.org/1999/xlink"
+      width="60px"
+      height="60px"
+      viewBox="0 0 271.317 271.318"
+      xml:space="preserve"
+    >
+      <g>
+        <g>
+          <path
+            d="M178.686,26.321c0,0-7.746,9.812-8.398,14.959c-1.036,8.093-2.62,20.485-2.62,20.485l-39.385,90.343
+			c0,0-1.064,6.824-11.309,1.31c-10.245-5.515,0.259-27.837-11.56-24.684c-11.819,3.153-18.124,19.697-15.762,42.543
+			c2.369,22.846-27.043,17.337-37.552,53.05c-10.499,35.719,25.476,38.345,39.657,43.601c14.188,5.25,45.699,7.881,53.837-19.165
+			c8.14-27.061-3.672-29.681,2.62-37.82c6.307-8.141,28.925-34.932,19.449-37.821c-9.466-2.89-33.096,16.285-31.781-2.356
+			c1.305-18.651,43.592-102.423,43.592-102.423s5.779-19.434,12.873-19.434c7.084,0,13.649-15.229,12.604-25.471
+			c-1.056-10.248,23.104-8.415,14.178-18.662C210.2-5.458,193.397,0.322,178.686,26.321z"
+          />
+        </g>
+      </g>
+    </svg>
     <nav>
       <ul>
         <li><RouterLink to="/">Home</RouterLink></li>
@@ -27,12 +51,16 @@ export default defineComponent({
 @import '../style/variables';
 
 header {
+  position: relative;
   height: 80px;
   background-color: $primary;
   display: flex;
   flex-direction: row;
   justify-content: space-between;
+  align-items: center;
   padding: 0 20px;
+  box-shadow: 0px 2px 5px black;
+  z-index: 2;
 }
 h1 {
   display: flex;
@@ -40,7 +68,7 @@ h1 {
   margin: 0;
 }
 nav {
-  width: 50%;
+  width: 70%;
 }
 ul {
   padding: 0;
